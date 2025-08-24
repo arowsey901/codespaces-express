6560ce6d51eb7df77015d6c4207a034320d0fe33
Ayeisha Rowsey <ayeisharowsey@student.purdueglobal.edu>
2025-08-24 21:48:43 +0000

    Refactor routes and update HTML file paths for services, about, contact, and FAQ pages; enhance index page content

--- index.js
+++ index.js
@@ -9,23 +9,28 @@ app.get('/', (req, res) => {
   res.send('Tech Bytes Repair')
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/services', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'services.html'));
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-
-app.get('/about', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'about.html'));
+app.get('/services', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'public/public/services.html'));
 })
 
-app.get('/faq', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'faq.html')); 
-
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'public/public/about.html'));
 })
 
-app.get('/contact', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+app.get('/faq', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'public/public/faq.html')); 
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'public/public/contact.html'));
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 
