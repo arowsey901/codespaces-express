f4e41e679a858a5dd96edad0de169e5545001248
Ayeisha Rowsey <ayeisharowsey@student.purdueglobal.edu>
2025-08-24 07:48:20 +0000

    Add routes for About, Services, FAQ, and Contact pages with corresponding HTML files

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ app.get('/', (req, res) => {
   res.send('Tech Bytes Repair')
 })
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/services', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'services.html'));
+})
+
+
+app.get('/about', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})
+
+app.get('/faq', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'faq.html')); 
+
+})
+
+app.get('/contact', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
