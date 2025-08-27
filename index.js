1433ee1fad2831c5f797581fa2f0e773af8dcbbb
Ayeisha Rowsey <ayeisharowsey@student.purdueglobal.edu>
2025-08-27 22:16:18 +0000

    Refactor server port configuration and remove unused middleware

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 const port = 3000;
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Tech Bytes Repair')
